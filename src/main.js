8cea52f69085a8a62b290a454d163e88e15b2774
sgdxbc <sung@comp.nus.edu.sg>
2025-01-23 13:35:09 +0800

    Checkpoint new data structure and rendering

--- src/main.js
+++ src/main.js
@@ -1,7 +1,97 @@
-import './style.css'
-import instructionHTML from "./instruction.html?raw"
+import './style.css';
+import instructionHTML from "./instruction.html?raw";
 
-const ui = { title: null, stats: null, view: null, viewStats: null, viewTimeTicks: null, viewNodes: null }
+function processLog(text) {
+  const logEvents = [];
+  let logOffset = 0;
+  const nodes = new Set();
+  for (let line of text.split('\n')) {
+    if (line === "") {
+      continue;
+    }
+    const reLevel = String.raw`\[(FINEST|FINER) *\]`;
+    const reTime = String.raw`\[(\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d\.\d{3})(\d{6})\]`;
+    const reSource = String.raw`\[([\w\.]*)\]`;
+    const reMessage = String.raw`(MessageSend|MessageReceive)\((\w+) -> (\w+), (\w+)\((.*)\)\)`;
+    const reTimer = String.raw`(TimerSet|TimerReceive)\(-> (\w+), (\w+)\((.*)\)\)`;
+    const reOther = String.raw`(.*)`;
+    const m = line.match(new RegExp(`^${reLevel} ${reTime} ${reSource} (${reMessage}|${reTimer}|${reOther})`));
+    if (m === null) {
+      // TODO warn unrecognized line
+      continue;
+    }
+
+    const level = m[1], dateString = m[2], subMillisecond = m[3], source = m[4], messageType = m[6], timerType = m[11];
+    const logTime = Date.parse(dateString) + Number.parseInt(subMillisecond) / 1_000_000;
+    if (logOffset === 0) {
+      logOffset = logTime;
+    }
+    const time = logTime - logOffset;
+    let event;
+    if (source === "dslabs.framework.Node") {
+      if (messageType !== undefined) {
+        const sendNode = m[7], receiveNode = m[8], name = m[9], data = m[10];
+        event = { time, type: messageType, sendNode, receiveNode, name, data };
+        nodes.add(sendNode);
+        nodes.add(receiveNode);
+      } else if (timerType !== undefined) {
+        const node = m[12], name = m[13], data = m[14];
+        event = { time, type: timerType, node, name, data };
+        nodes.add(node);
+      } else {
+        // TODO warn invalid log
+        continue;
+      }
+    } else {
+      const content = m[15];
+      event = { time, type: "*", level, content };
+    }
+    // console.log(event);
+    logEvents.push(event);
+  }
+
+  const viewSpans = [];
+  const nodeLastSpans = new Map();
+  const inflightEvents = [];
+  for (const event of logEvents) {
+    if (event.type === "MessageReceive" || event.type === "TimerReceive") {
+      const span = { endTime: Infinity, ...event };
+      viewSpans.push(span);
+
+      const node = event.type === "MessageReceive" ? event.receiveNode : event.node;
+      if (nodeLastSpans.get(node) !== undefined) {
+        nodeLastSpans.get(node).endTime = event.time;
+      }
+      nodeLastSpans.set(node, span);
+
+      for (const [i, inflightEvent] of inflightEvents.entries()) {
+        // is this condition too loose?
+        if (inflightEvent.name === event.name && inflightEvent.data === event.data) {
+          const sourceNode = inflightEvent.type === "MessageSend" ? inflightEvent.sendNode : inflightEvent.node;
+          viewSpans.push({
+            type: "->",
+            time: inflightEvent.time, endTime: event.time,
+            sourceNode, destinationNode: node,
+            name: event.name, data: event.data
+          });
+          inflightEvents.splice(i, 1);
+        }
+      }
+    } else {
+      viewSpans.push({ endTime: event.time, ...event });
+      if (event.type === "MessageSend" || event.type === "TimerSet") {
+        inflightEvents.push(event);
+      }
+    }
+  }
+
+  viewSpans.sort((s1, s2) => s1.time - s2.time);
+  const endIndexes = viewSpans.entries().map(([i, span]) => { return { endTime: span.endTime, index: i }; }).toArray();
+  endIndexes.sort((s1, s2) => s1.endTime - s2.endTime);
+  return { spans: viewSpans, endSpans: endIndexes, offset: logOffset, nodes: new Array(...nodes) };
+}
+
+const ui = { title: null, stats: null, view: null, viewStats: null, viewTimeTicks: null, viewSpans: null };
 
 let logName = "lab0-test1.txt";
 let logContent =
@@ -54,7 +144,7 @@ function createUI() {
     fileReader.readAsText(file);
   });
 
-  const instruction = document.createElement('div');  
+  const instruction = document.createElement('div');
   app.appendChild(instruction);
   instruction.innerHTML = instructionHTML;
 
@@ -67,6 +157,7 @@ function createUI() {
 
   ui.viewStats = document.createElement('div');
   ui.view.appendChild(ui.viewStats);
+  ui.viewStats.id = "view-stats";
 
   const viewTime = document.createElement('div');
   ui.view.appendChild(viewTime);
@@ -76,10 +167,9 @@ function createUI() {
 
   ui.viewTimeTicks = document.createElement('div');
   viewTime.appendChild(ui.viewTimeTicks);
-  ui.viewTimeTicks.className = "timeline-container";
 
-  ui.viewNodes = document.createElement('div');
-  ui.view.appendChild(ui.viewNodes);
+  ui.viewSpans = document.createElement('div');
+  ui.view.appendChild(ui.viewSpans);
 
   document.addEventListener('keydown', (event) => {
     if (event.key.startsWith('Arrow')) {
@@ -91,168 +181,52 @@ function createUI() {
   renderUI();
 }
 
-function processLog(text) {
-  const events = [];
-  let logOffset = 0;
-  const nodes = new Set();
-  const nodeSpans = new Map();
-  const inflightMessageSend = [];
-  const inflightTimerSet = [];
-  for (let line of text.split('\n')) {
-    if (line === "") {
-      continue;
-    }
-    const reLevel = String.raw`\[(FINEST|FINER) *\]`;
-    const reTime = String.raw`\[(\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d\.\d{3})(\d{6})\]`;
-    const reSource = String.raw`\[([\w\.]*)\]`;
-    const reMessage = String.raw`(MessageSend|MessageReceive)\((\w+) -> (\w+), (\w+)\((.*)\)\)`;
-    const reTimer = String.raw`(TimerSet|TimerReceive)\(-> (\w+), (\w+)\((.*)\)\)`;
-    const reOther = String.raw`(.*)`;
-    const re = new RegExp(`^${reLevel} ${reTime} ${reSource} (${reMessage}|${reTimer}|${reOther})`);
-    const m = line.match(re);
-    if (m === null) {
-      // TODO warn unrecognized line
-      continue;
-    }
-
-    const level = m[1], dateString = m[2], subMillisecond = m[3], source = m[4], messageType = m[6], timerType = m[11];
-    const logTime = Date.parse(dateString) + Number.parseInt(subMillisecond) / 1_000_000;
-    if (logOffset === 0) {
-      logOffset = logTime;
-    }
-    const time = logTime - logOffset;
-    let event;
-    if (source === "dslabs.framework.Node") {
-      if (messageType !== undefined) {
-        const sendNode = m[7], receiveNode = m[8], name = m[9], data = m[10];
-        event = { time, type: messageType, sendNode, receiveNode, name, data };
-        nodes.add(sendNode);
-        nodes.add(receiveNode);
-      } else if (timerType !== undefined) {
-        const node = m[12], name = m[13], data = m[14];
-        event = { time, type: timerType, node, name, data };
-        nodes.add(node);
-      } else {
-        // TODO warn invalid log
-        continue;
-      }
-    } else {
-      const content = m[15];
-      event = { time, type: "*", level, content };
-    }
-    // console.log(event);
-
-    let index = events.length;
-    events.push(event);
-
-    let node = null;
-    if (event.type === "MessageReceive") {
-      node = event.receiveNode;
-    }
-    if (event.type === "TimerReceive") {
-      node = event.node;
-    }
-    if (node !== null) {
-      const lastIndex = nodeSpans.get(node);
-      if (lastIndex !== undefined) {
-        events[lastIndex].timeEnd = event.time;
-        event.previousIndex = lastIndex;
-      }
-      nodeSpans.set(node, index);
-    }
-
-    // if there are multiple identical inflight events, assuming they are arriving in order
-    if (event.type === "MessageSend") {
-      inflightMessageSend.push(event);
-    }
-    if (event.type === "TimerSet") {
-      inflightTimerSet.push(event);
-    }
-    if (event.type === "MessageReceive") {
-      for (const [index, inflightEvent] of inflightMessageSend.entries()) {
-        if (
-          inflightEvent.sendNode === event.sendNode
-          && inflightEvent.receiveNode == event.receiveNode
-          && inflightEvent.name == event.name
-          && inflightEvent.data == inflightEvent.data
-        ) {
-          event.causedBy = inflightEvent;
-          inflightEvent.become = event;
-          inflightMessageSend.splice(index, 1);
-          break;
-        }
-      }
-    }
-    if (event.type === "TimerReceive") {
-      for (const [index, inflightEvent] of inflightTimerSet.entries()) {
-        if (
-          inflightEvent.node === event.node
-          && inflightEvent.name == event.name
-          && inflightEvent.data == inflightEvent.data
-        ) {
-          event.causedBy = inflightEvent;
-          inflightEvent.become = event;
-          inflightTimerSet.splice(index, 1);
-          break;
-        }
-      }
-    }
-  }
-
-  for (const index of nodeSpans.values()) {
-    events[index].timeEnd = Infinity;
-    // no span keeps track of these last spans so they won't appear when their "tails" are in the view
-    // should be minor, but still would be good to be solved
-  }
-  return { events, offset: logOffset, nodes: new Array(...nodes) };
-}
-
-let viewStart = 0, viewEnd = 25;
-let nodeElements = new Map();
-let eventElements = new Map();
-let eventElementsStart = 0, eventElementsEnd = 0;
+let viewStartTime = -1, viewEndTime = -1;
+let nodeRows = null;
+let spanElements = null;
+let maxStartIndex = -1, minEndIndex = -1;
 
 // calling once per loading log file
 function renderUI() {
   ui.title.innerHTML = `<strong>DSLabs Log Visualizer:</strong> ${logName}`;
 
-  const duration = logContent.events.length === 0 ? 0 : Math.round(logContent.events[logContent.events.length - 1].time);
+  let duration = 0;
+  for (let i = logContent.endSpans.length; i > 0; i -= 1) {
+    const endTime = logContent.endSpans[i - 1].endTime;
+    if (endTime === Infinity) {
+      continue;
+    }
+    duration = endTime;
+    break;
+  }
   ui.stats.innerHTML = `<strong>Start</strong> ${new Date(logContent.offset).toLocaleString()} <strong>Duration</strong> ${duration}ms (processing the last message/timer may take a bit more)`;
 
-  ui.viewNodes.innerHTML = "";
-  nodeElements = new Map();
-  for (const nodeName of logContent.nodes) {
+  ui.viewSpans.innerHTML = "";
+  nodeRows = new Map();
+  for (const [index, nodeName] of logContent.nodes.entries()) {
+    nodeRows.set(nodeName, index);
+
     const node = document.createElement('div');
-    ui.viewNodes.appendChild(node);
+    ui.viewSpans.appendChild(node);
     node.className = "row";
-
-    const nodeEvents = document.createElement('div');
-    node.appendChild(nodeEvents);
-    nodeEvents.className = "timeline-container";
-
-    const label = document.createElement('div');
-    node.appendChild(label);
-    label.className = "node-label";
-    label.innerText = nodeName;
-
-    nodeElements.set(nodeName, nodeEvents);
+    node.innerText = nodeName;
   }
 
-  viewStart = 0;
-  viewEnd = 25;
-  eventElements = new Map();
-  eventElementsStart = 0;
-  eventElementsEnd = 0;
+  viewStartTime = 0;
+  viewEndTime = 25;
+  maxStartIndex = 0;
+  minEndIndex = 0;
+  spanElements = new Map();
   renderView();
 }
 
 // calling once per user interaction (e.g. adjust view)
 function renderView() {
-  const durationMillis = viewEnd - viewStart;
+  const durationMillis = viewEndTime - viewStartTime;
 
   ui.viewTimeTicks.innerHTML = "";
   const tickMillis = Math.max(Math.floor(durationMillis / 10), 1);
-  for (let time = Math.ceil(viewStart / tickMillis) * tickMillis; time < viewEnd; time += tickMillis) {
+  for (let time = Math.ceil(viewStartTime / tickMillis) * tickMillis; time < viewEndTime; time += tickMillis) {
     const tick = document.createElement('div');
     ui.viewTimeTicks.appendChild(tick);
     tick.className = "tick";
@@ -261,134 +235,111 @@ function renderView() {
   }
 
   let i;
-  for (i = eventElementsStart; i < eventElementsEnd; i += 1) {
-    if (logContent.events[i].time >= viewStart) {
+  for (i = maxStartIndex; i < logContent.spans.length; i += 1) {
+    const span = logContent.spans[i];
+    if (span.time >= viewEndTime) {
       break;
     }
-    eventElements.get(i).remove();
-    eventElements.delete(i);
+    const element = createSpanElement(span);
+    ui.viewSpans.appendChild(element);
+    spanElements.set(i, element);
   }
-  eventElementsStart = i;
-  for (; i < eventElementsEnd; i += 1) {
-    const event = logContent.events[i];
-    if (event.time >= viewEnd) {
+  maxStartIndex = i;
+  for (i = minEndIndex; i > 0; i -= 1) {
+    const endSpan = logContent.endSpans[i - 1];
+    if (endSpan.endTime < viewStartTime) {
       break;
     }
-    eventElements.get(i).style.left = styleLeft(event.time);
-    if (event.timeEnd !== undefined) {
-      eventElements.get(i).style.width = styleWidth(event.time, event.timeEnd);
+    if (spanElements.has(endSpan.index)) {
+      console.warn("Duplicated rendering span", logContent.spans[endSpan.index]);
+      continue;
     }
+    const element = createSpanElement(logContent.spans[endSpan.index]);
+    ui.viewSpans.appendChild(element);
+    spanElements.set(endSpan.index, element);
   }
-  const j = i;
-  for (; i < eventElementsEnd; i += 1) {
-    eventElements.get(i).remove();
-    eventElements.delete(i);
-  }
-  eventElementsEnd = j;
+  minEndIndex = i;
 
-  for (i = eventElementsEnd; i < logContent.events.length; i += 1) {
-    const event = logContent.events[i];
-    if (event.time >= viewEnd) {
-      break;
-    }
-    const eventElement = createEventElement(event);
-    appendEventElement(event, eventElement);
-
-    eventElements.set(i, eventElement);
-  }
-  eventElementsEnd = i;
-  for (i = eventElementsStart; i > 0; i -= 1) {
-    const event = logContent.events[i - 1];
-    if (event.time < viewStart) {
-      break;
-    }
-    const eventElement = createEventElement(event);
-    appendEventElement(event, eventElement);
-
-    eventElements.set(i - 1, eventElement);
-  }
-  eventElementsStart = i;
-
-  ui.viewStats.innerHTML = `<strong>View Start</strong> ${viewStart}ms <strong>Duration</strong> ${durationMillis}ms`;
+  ui.viewStats.innerHTML = `<strong>View Start</strong> ${viewStartTime}ms <strong>Duration</strong> ${durationMillis}ms`;
 }
 
 function styleLeft(time) {
-  return `${(time - viewStart) / (viewEnd - viewStart) * 100}%`;
+  return `${(time - viewStartTime) / (viewEndTime - viewStartTime) * 90 + 5}%`;
+}
+
+function styleTop(row, offset = 0) {
+  return `${row * 173 + 170 + offset}px`;
 }
 
 function styleWidth(time, timeEnd) {
-  return timeEnd === Infinity ? "999px" : `calc(${(timeEnd - time) / (viewEnd - viewStart) * 100}% - 10px)`;
+  return timeEnd === Infinity ? "999px" : `calc(${(timeEnd - time) / (viewEndTime - viewStartTime) * 100}% - 10px)`;
 }
 
 function updateViewTimeRange(key) {
   if (key === "ArrowLeft") {
-    const offset = Math.min((viewEnd - viewStart) * 0.1, viewStart);
-    viewStart -= offset;
-    viewEnd -= offset;
+    const offset = Math.min((viewEndTime - viewStartTime) * 0.1, viewStartTime);
+    viewStartTime -= offset;
+    viewEndTime -= offset;
   }
   if (key === "ArrowRight") {
-    const offset = (viewEnd - viewStart) * 0.1;
-    viewStart += offset;
-    viewEnd += offset;
+    const offset = (viewEndTime - viewStartTime) * 0.1;
+    viewStartTime += offset;
+    viewEndTime += offset;
   }
   if (key === "ArrowUp") {
-    const offset = (viewEnd - viewStart) * 0.33;
-    viewStart += offset / 2;
-    viewEnd -= offset / 2;
+    const offset = (viewEndTime - viewStartTime) * 0.33;
+    viewStartTime += offset / 2;
+    viewEndTime -= offset / 2;
   }
   if (key === "ArrowDown") {
-    const offset = (viewEnd - viewStart) * 0.5;
-    const leftOffset = Math.min(offset / 2, viewStart);
-    viewStart -= leftOffset;
-    viewEnd += offset - leftOffset;
+    const offset = (viewEndTime - viewStartTime) * 0.5;
+    const leftOffset = Math.min(offset / 2, viewStartTime);
+    viewStartTime -= leftOffset;
+    viewEndTime += offset - leftOffset;
   }
   renderView();
 }
 
-function createEventElement(event) {
+function createSpanElement(span) {
   const eventElement = document.createElement('div');
-  eventElement.style.left = styleLeft(event.time);
+  eventElement.style.left = styleLeft(span.time);
 
-  if (event.type.endsWith("Receive")) {
+  if (span.type.endsWith("Receive")) {
     eventElement.className = "span";
-    eventElement.innerText = event.name;
-    eventElement.style.width = styleWidth(event.time, event.timeEnd);
-    eventElement.style.background = `linear-gradient(to left, hsl(0 0 0 / 0), ${colorByName(event.name)} max(50px, 20%))`;
+    eventElement.innerText = span.name;
+    eventElement.style.width = styleWidth(span.time, span.endTime);
+    eventElement.style.background = `linear-gradient(to left, hsl(0 0 0 / 0), ${colorByName(span.name)} max(50px, 20%))`;
+  } else if (span.type === "->") {
+
   } else {
     eventElement.className = "event";
 
     const icon = document.createElement('div');
     eventElement.appendChild(icon);
-    icon.style.color = `hsl(from ${colorByName(event.name)} h s calc(l - 10))`;
+    icon.style.color = `hsl(from ${colorByName(span.name)} h s calc(l - 10))`;
     icon.classList.add("fa-solid");
-    if (event.type === "MessageSend") {
-      icon.classList.add("fa-envelope");
-    }
-    if (event.type === "TimerSet") {
-      icon.classList.add("fa-clock");
-    }
-
     const name = document.createElement('div');
     eventElement.appendChild(name);
-    name.innerText = event.name;
+    name.innerText = span.name;
+
+    if (span.type === "MessageSend") {
+      icon.classList.add("fa-envelope");
+    }
+    if (span.type === "TimerSet") {
+      icon.classList.add("fa-clock");
+    }
+  }
+
+  if (span.type === "MessageReceive") {
+    eventElement.style.top = styleTop(nodeRows.get(span.receiveNode));
+  }
+  if (span.type === "MessageSend") {
+    eventElement.style.top = styleTop(nodeRows.get(span.sendNode));
+  }
+  if (span.type.startsWith("Timer")) {
+    eventElement.style.top = styleTop(nodeRows.get(span.node));
   }
   return eventElement;
-}
-
-function appendEventElement(event, eventElement) {
-  let node = null;
-  if (event.type === "MessageReceive") {
-    node = event.receiveNode;
-  }
-  if (event.type === "MessageSend") {
-    node = event.sendNode;
-  }
-  if (event.type.startsWith("Timer")) {
-    node = event.node;
-  }
-  if (node !== null) {
-    nodeElements.get(node).appendChild(eventElement);
-  }
 }
 
 // https://stackoverflow.com/a/34842797
